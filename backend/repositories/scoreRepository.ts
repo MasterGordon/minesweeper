ad0d82ab1c578c25ae181a5ec350d9a71d00387b
MasterGordon <gologo.x@gmail.com>
2024-09-29 14:26:45 +0200

    fixed scoreboard sorting

--- backend/repositories/scoreRepository.ts
+++ backend/repositories/scoreRepository.ts
@@ -3,9 +3,11 @@ import { Game } from "../schema";
 import type { BunSQLiteDatabase } from "drizzle-orm/bun-sqlite";
 
 export const getScoreBoard = async (db: BunSQLiteDatabase) => {
-  return await db
-    .select({ stage: sql<number>`max(${Game.stage})`, user: Game.user })
-    .from(Game)
-    .where(not(eq(Game.finished, 0)))
-    .groupBy(Game.user);
+  return (
+    await db
+      .select({ stage: sql<number>`max(${Game.stage})`, user: Game.user })
+      .from(Game)
+      .where(not(eq(Game.finished, 0)))
+      .groupBy(Game.user)
+  ).sort((a, b) => b.stage - a.stage);
 };
