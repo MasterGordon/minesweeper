881d40a642681735b560972304e28cfa41f3c259
CutieCat2804 <cutiecattv@gmail.com>
2024-10-05 15:06:12 +0200

    add theme package 3

---
+++ src/themes/halli-galli.ts
@@ -0,0 +1,19 @@
+import { Theme } from "./Theme";
+
+export const halliGalliTheme: Theme = {
+  size: 32,
+  mine: () => import("../assets/themes/halli-galli/mine.png"),
+  tile: () => import("../assets/themes/halli-galli/tile.png"),
+  revealed: () => import("../assets/themes/halli-galli/revealed.png"),
+  flag: () => import("../assets/themes/halli-galli/flag.png"),
+  questionMark: () => import("../assets/themes/halli-galli/question-mark.png"),
+  lastPos: () => import("../assets/themes/halli-galli/last-pos.png"),
+  1: () => import("../assets/themes/halli-galli/1.png"),
+  2: () => import("../assets/themes/halli-galli/2.png"),
+  3: () => import("../assets/themes/halli-galli/3.png"),
+  4: () => import("../assets/themes/halli-galli/4.png"),
+  5: () => import("../assets/themes/halli-galli/5.png"),
+  6: () => import("../assets/themes/halli-galli/6.png"),
+  7: () => import("../assets/themes/halli-galli/7.png"),
+  8: () => import("../assets/themes/halli-galli/8.png"),
+};
---
+++ src/themes/isaac.ts
@@ -0,0 +1,19 @@
+import { Theme } from "./Theme";
+
+export const isaacTheme: Theme = {
+  size: 32,
+  mine: () => import("../assets/themes/isaac/mine-3.png"),
+  tile: () => import("../assets/themes/isaac/tile.png"),
+  revealed: () => import("../assets/themes/isaac/revealed.png"),
+  flag: () => import("../assets/themes/isaac/flag.png"),
+  questionMark: () => import("../assets/themes/isaac/question-mark.png"),
+  lastPos: () => import("../assets/themes/isaac/last-pos.png"),
+  1: () => import("../assets/themes/isaac/1.png"),
+  2: () => import("../assets/themes/isaac/2.png"),
+  3: () => import("../assets/themes/isaac/3.png"),
+  4: () => import("../assets/themes/isaac/4.png"),
+  5: () => import("../assets/themes/isaac/5.png"),
+  6: () => import("../assets/themes/isaac/6.png"),
+  7: () => import("../assets/themes/isaac/7.png"),
+  8: () => import("../assets/themes/isaac/8.png"),
+};
