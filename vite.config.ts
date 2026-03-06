de4400c52ff08165302a78280bf6ba276da6d4b5
MasterGordon <gologo.x@gmail.com>
2026-03-06 22:22:23 +0100

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { imagetools } from "vite-imagetools";
 // import { analyzer } from "vite-bundle-analyzer";
